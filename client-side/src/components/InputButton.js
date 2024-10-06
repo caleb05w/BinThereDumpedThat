09e2dd735c5c1231027ea3d0138454555924f111
Brandon Sun <brandonsun75@gmail.com>
2024-10-06 02:09:03 -0700

    CreateBinUI

--- client-side/src/components/InputButton.js
+++ client-side/src/components/InputButton.js
@@ -4,7 +4,7 @@ const InputButton = ({placeholder, onChange, value, type}) => {
   return (
         <input className='border-2 border-black w-[100%] py-[1.2%] rounded-[0.25rem] px-[1%]'
               placeholder= {placeholder}
-              onChange={{onChange}}
+              onChange={onChange}
               value={value}
               type={type}
             />
